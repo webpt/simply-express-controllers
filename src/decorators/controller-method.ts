c409492d7486eb5534735ba5ded60ab2a98ddb57
William Matthews <robophred@gmail.com>
2019-07-23 08:25:01 -0700

    Documentation

--- src/decorators/controller-method.ts
+++ src/decorators/controller-method.ts
@@ -1,12 +1,42 @@
+import { JSONSchema6 } from "json-schema";
+
 import {
   appendControllerMethodMetadata,
   ControllerMethodArgMetadata
 } from "../metadata/controller-method";
 
+/**
+ * Describes common settings for controller methods.
+ */
 export interface ControllerMethodSettings {
+  /**
+   * The path of this method, relative to the controller path.
+   */
   path?: string;
+  /**
+   * Query parameter definitions.
+   */
+  queryParams?: Record<string, QueryParamSettings>;
 }
 
+/**
+ * Describes a query parameter.
+ */
+export interface QueryParamSettings extends Omit<JSONSchema6, "required"> {
+  /**
+   * The type of this parameter.
+   */
+  type: "string" | "number" | "boolean";
+  /**
+   * Whether this parameter is required.
+   */
+  required?: boolean;
+}
+
+/**
+ * Annotates this method to be a GET request method.
+ * @param settings Settings for this request method.
+ */
 export function get(settings: ControllerMethodSettings): MethodDecorator {
   return (target: any, propertyKey: string | symbol) => {
     appendControllerMethodMetadata(target[propertyKey], {
@@ -16,7 +46,11 @@ export function get(settings: ControllerMethodSettings): MethodDecorator {
   };
 }
 
-export function post(settings: any): MethodDecorator {
+/**
+ * Annotates this method to be a POST request method.
+ * @param settings Settings for this request method.
+ */
+export function post(settings: ControllerMethodSettings): MethodDecorator {
   return (target: any, propertyKey: string | symbol) => {
     appendControllerMethodMetadata(target[propertyKey], {
       method: "POST",
@@ -25,6 +59,9 @@ export function post(settings: any): MethodDecorator {
   };
 }
 
+/**
+ * Annotates this parameter to receive the request body.
+ */
 export function body(): ParameterDecorator {
   return (target: any, propertyKey: string | symbol, methodIndex: number) => {
     const partialArgs: ControllerMethodArgMetadata[] = [];
@@ -37,6 +74,13 @@ export function body(): ParameterDecorator {
   };
 }
 
+/**
+ * Annotates this parameter to receive the given path parameter.
+ *
+ * If the path parameter has a definition in the method decorator,
+ * it will be cocerced to the type supplied in that definition.
+ * Otherwise, it will be a string.
+ */
 export function pathParam(paramName: string): ParameterDecorator {
   return (target: any, propertyKey: string | symbol, methodIndex: number) => {
     const partialArgs: ControllerMethodArgMetadata[] = [];
@@ -50,12 +94,19 @@ export function pathParam(paramName: string): ParameterDecorator {
   };
 }
 
-export function paramName(paramName: string): ParameterDecorator {
+/**
+ * Annotates this parameter to receive the given query parameter.
+ *
+ * If the path parameter has a definition in the method decorator,
+ * it will be cocerced to the type supplied in that definition.
+ * Otherwise, it will be a string.
+ */
+export function queryParam(paramName: string): ParameterDecorator {
   return (target: any, propertyKey: string | symbol, methodIndex: number) => {
     const partialArgs: ControllerMethodArgMetadata[] = [];
     partialArgs[methodIndex] = {
       type: "queryParam",
-      paramName
+      paramName: paramName
     };
     appendControllerMethodMetadata(target[propertyKey], {
       args: partialArgs
--- src/decorators/controller.ts
+++ src/decorators/controller.ts
@@ -1,8 +1,20 @@
 import { setControllerMetadata } from "../metadata/controller";
 
+/**
+ * Settings describing a route controller.
+ */
 export interface ControllerSettings {
+  /**
+   * The request path of this controller.
+   * All request methods will be relative to this path.
+   */
   path?: string;
 }
+
+/**
+ * Annotates this class as being a controller.
+ * @param settings The controller settings.
+ */
 export function controller(settings: any): ClassDecorator {
   return (target: any) => {
     setControllerMetadata(target, settings);
