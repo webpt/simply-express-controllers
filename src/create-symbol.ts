9e44416b0b3f2d3ffb0af7a44328aef2143fbace
RoboPhred <robophred@gmail.com>
2021-07-21 11:43:07 -0700

    Use github repo for symbol prefix

--- src/create-symbol.ts
+++ src/create-symbol.ts
@@ -1,4 +1,4 @@
-const URI_PREFIX = "com.webpt.soapdish.controllers";
+const URI_PREFIX = "https://github.com/webpt/simply-express-controllers#";
 export default function createSymbol(...name: (string | string[])[]): symbol {
   const fullName = ([] as string[]).concat(...name);
   return Symbol.for(`${URI_PREFIX}:${fullName}`);
